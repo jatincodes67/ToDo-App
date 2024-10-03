7f2221a0a1061045deb9a83559c0db3b03c199b4
jatincodes67 <jagsol98@gmail.com>
2024-10-03 14:54:43 +0530

    edited store

--- src/store/items-store.jsx
+++ src/store/items-store.jsx
@@ -1,6 +1,4 @@
 import { createContext } from "react";
-import itemsStoreContext from "../store/items-store-Context.js";
-
 const itemsStoreContext = createContext([]); // Create a context object
 
 export default itemsStoreContext; // Exporting as default
